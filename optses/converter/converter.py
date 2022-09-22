df0e86322083c39e456233312c728edaaab54986
Martin Cornejo <martin.cornejo@tum.de>
2022-09-23 00:11:43 +0200

    Update QuadFitLoss and Rampinelli/Notton converter models

--- optses/converter/converter.py
+++ optses/converter/converter.py
@@ -79,22 +79,29 @@ class LinearFitConverter(
 class QuadraticFitConverter(AbstractConverter):
     def __init__(self, power, k0, k1, k2) -> None:
         self._power = power
-        self._k0 = k0
+        # TODO: kwarg to specify scaling?
+        self._k0 = k0 * power
         self._k1 = k1
-        self._k2 = k2
+        self._k2 = k2 / power
 
     def build(self, block) -> None:
+        model = block.model()
+
         block.k0 = opt.Param(within=opt.Reals, initialize=self._k0)
         block.k1 = opt.Param(within=opt.Reals, initialize=self._k1)
         block.k2 = opt.Param(within=opt.Reals, initialize=self._k2)
 
         block.pemax = opt.Param(within=opt.NonNegativeReals, initialize=self._power)
 
-        block.power = opt.Var(block.time)
+        block.power = opt.Var(model.time)
 
-        @block.Constraint(block.time)
-        def converter_efficiency(b, t):
-            return b.power_dc[t] == b.k0 + b.k1 * b.power[t] + b.k2 * b.power[t] ** 2
+        @block.Expression(model.time)
+        def converter_loss(b, t):
+            return b.k0 + b.k1 * b.power[t] + b.k2 * b.power[t] ** 2
+
+        @block.Constraint(model.time)
+        def converter_loss_constraint(b, t):
+            return b.power_dc[t] <= b.power[t] - b.converter_loss[t]
 
 
 class RampinelliFitConverter(AbstractConverter):
@@ -105,22 +112,34 @@ class RampinelliFitConverter(AbstractConverter):
         self._k2 = k2
 
     def build(self, block):
-        block.k0 = opt.Param(
-            within=opt.Reals, initialize=self._k0
-        )  # charge and discharge params?
+        model = block.model()
+
+        # charge and discharge params?
+        block.k0 = opt.Param(within=opt.Reals, initialize=self._k0)  
         block.k1 = opt.Param(within=opt.Reals, initialize=self._k1)
         block.k2 = opt.Param(within=opt.Reals, initialize=self._k2)
 
         block.pemax = opt.Param(within=opt.NonNegativeReals, initialize=self._power)
+        block.pec = opt.Var(model.time, within=opt.Reals, bounds=(0, block.pemax))
+        block.ped = opt.Var(model.time, within=opt.Reals, bounds=(0, block.pemax))
 
-        # TODO:
+        @block.Expression(model.time)
+        def c_effc(b, t):
+            p = b.pec[t] / b.pemax
+            return p / (p + b.k0 + b.k1*p + b.k2*p ** 2)
 
-        # @block.Constraint(block.time)
-        # def converter_efficiency(b, t):
+        @block.Expression(model.time)
+        def c_effd(b, t):
+            p = b.ped[t] / b.pemax
+            return p / (p + b.k0 + b.k1*p + b.k2*p ** 2)
 
-        # pr = pe/pmax
-        # eff = pr / (pr + k0 + k1*pr + k2*pr**2)
-        # p = pe * eff ?
+        @block.Constraint(model.time)
+        def converter_efficiency_constraint(b, t):
+            return b.power_dc[t] == b.pec[t] * b.c_effc[t] - b.ped[t] * (1 / b.c_effd[t])
+
+        @block.Expression(model.time)
+        def power(b, t):
+            return b.pec[t] - b.ped[t]
 
 
 class NottonFitConverter(RampinelliFitConverter):
